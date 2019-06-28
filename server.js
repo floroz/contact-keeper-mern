805593a7150f8f3c158f101d7d5ff762c1013208
Daniele <tortora87@gmail.com>
2019-06-28 22:39:48 +0100

    set up auth and middleware for users and contacts

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to my Server' }));
+// Connect Database
+connectDB();
+
+// Init middlewere
+app.use(express.json({ extended: false }));
 
 // Routes
 app.use('/api/users', require('./routes/users'));
